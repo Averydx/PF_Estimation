eb41846546a3ab8fcb3c61584ecbb4ad4f2f761a
Averydx <79957750+Averydx@users.noreply.github.com>
2023-07-22 12:04:00 -0700

    Update Filtering.py

--- Filtering.py
+++ Filtering.py
@@ -1,6 +1,7 @@
 import numpy as np; 
 import pandas as pd; 
 import NumericalPropagator; 
+from scipy.special import gamma; 
 
 class ParticleFilter: 
     def __init__(self,initial_state,beta_prior,num_particles,filePath):
@@ -9,6 +10,7 @@ class ParticleFilter:
         self.particles = []; 
         self.weights = np.ones(shape=num_particles); 
         self.Propagrator = NumericalPropagator.one_step_propagator(initial_state);
+        self.dailyInfected = np.zeros(shape=num_particles); 
         self.gamma = 0.04; 
         for i in range(len(self.weights)): 
              self.weights[i] /= num_particles;  
@@ -25,24 +27,41 @@ class ParticleFilter:
 
     def estimate_params(self): 
          for t in range(1): 
-             for i in range(len(self.particles)): 
+            self.propagate(); 
+            self.compute_temp_weights(t); 
+
+
+
+    #internal helper function to propagate the particle cloud
+    def propagate(self): 
+        for i in range(len(self.particles)): 
                 self.Propagrator.params = [self.particles[i][1],self.gamma];
-                print(self.particles[i][0]); 
                 self.Propagrator.state = self.particles[i][0]; 
-                print(self.Propagrator.propagate()); 
+                temp = self.Propagrator.propagate(); 
+                self.particles[i][0]= temp[0]; 
+                self.dailyInfected[i] = temp[1]; 
+        #print((self.particles))
+
+
 
     #internal helper function to compute weights based on observations
+    def compute_temp_weights(self,t):
+        temp_weights = np.zeros(len(self.particles)); 
+        for j in range(len(self.observation_data)): 
+                a = self.weights[j] * (self.dailyInfected[j] ** self.observation_data[t+1]); 
+                a = a/gamma(self.observation_data[t+1]); 
+                a = np.exp(-self.dailyInfected[j]); 
+                
+                temp_weights[j] = a; 
+
+
+        temp_weights = temp_weights/len(temp_weights); 
+
+
+
 
 
 
 
 
                  
-
-
-
-
-
-
-
-
