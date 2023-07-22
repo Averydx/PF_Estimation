19b4e16ee96654df945c7853a621f11994bbbb9b
Averydx <79957750+Averydx@users.noreply.github.com>
2023-07-21 17:35:48 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+
+import Filtering; 
+import pandas as pd; 
+import numpy as np; 
+import matplotlib.pyplot as plt; 
+
+def main():
+    state = [10000,10,0]; 
+    pf = Filtering.ParticleFilter(beta_prior=[0.,0.3],
+                                  initial_state=state,
+                                  num_particles=1000, 
+                                  filePath="observations.csv");
+
+    pf.estimate_params()
+
+
+
+
+    
