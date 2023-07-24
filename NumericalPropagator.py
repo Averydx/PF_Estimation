cb88e5cd24c1830c1206c55f1ff3912913c16261
Averydx <79957750+Averydx@users.noreply.github.com>
2023-07-23 21:13:58 -0700

    Update NumericalPropagator.py

--- NumericalPropagator.py
+++ NumericalPropagator.py
@@ -8,6 +8,37 @@ class one_step_propagator:
         self.state = None;
         self.params = None; 
 
+    def RHS(self,state,params):
+    #params has all the parameters – beta, gamma
+    #state is a numpy array
+
+        S,I,R = state; 
+
+        print(state); 
+
+        dS = -params[0]*S*I
+        dI = params[0]*S*I-params[1]*I
+        dR = params[1]*I
+
+        return np.array([dS,dI,dR])
+    
+    def propagate_euler(self): 
+         #define time step of Euler's method
+
+        NperDay = 10
+        sol = np.zeros((3, NperDay+1))
+        tSpanFine = np.linspace(0, 1, NperDay+1)
+
+        #initiate the values of SIR compartments at the first time point
+        sol[:,0] = np.array([self.state[0], self.state[1], self.state[2]])
+
+        for i in range(len(tSpanFine)-1):
+            sol[:,i+1] = sol[:,i] + self.RHS(sol[:,i], self.params)/NperDay; 
+            
+        return sol[:,-1]
+
+
+
     def propagate(self): 
         beta = self.params[0]; 
         gamma = self.params[1]; 
