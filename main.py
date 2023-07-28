6941449fbc859ace04c2579126492caadbd5d400
Averydx <79957750+Averydx@users.noreply.github.com>
2023-07-28 08:44:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 
+
 import Filtering; 
 import pandas as pd; 
 import numpy as np; 
 import matplotlib.pyplot as plt; 
 
 def main():
-    state = [10000,10,0]; 
-    pf = Filtering.ParticleFilter(beta_prior=[0.,0.5],
+    state = [10000,100,0]; 
+    pf = Filtering.ParticleFilter(beta_prior=[0.,1.],
                                   initial_state=state,
-                                  num_particles=1000, 
-                                  filePath="observations.csv");
+                                  num_particles=100, 
+                                  filePath="observations_euler.csv");
 
-    pf.estimate_params(50);
+    betas = pf.estimate_params(1);
+    plt.plot(betas);
+    plt.show();
     
 
 if __name__ == "__main__":
