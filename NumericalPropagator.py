547bc684831b0b0ed2cae4ee3c9e5cf508ce639d
Averydx <79957750+Averydx@users.noreply.github.com>
2023-07-28 08:44:12 -0700

    Update NumericalPropagator.py

--- NumericalPropagator.py
+++ NumericalPropagator.py
@@ -8,12 +8,14 @@ class one_step_propagator:
         self.state = None;
         self.params = None; 
 
-    def RHS(state,params):
+    def RHS(self,state,params):
     #params has all the parameters – beta, gamma
     #state is a numpy array
 
         S,I,R = state;
         N = S + I + R; 
+
+        new_I = params[0]*S*I/N;
 
         dS = -params[0]*(S*I)/N
         dI = params[0]*S*I/N-params[1]*I
@@ -21,23 +23,31 @@ class one_step_propagator:
 
 
 
-        return np.array([dS,dI,dR])
-    
-    def propagate_euler(self): 
+        return np.array([dS,dI,dR]),new_I
+
+    def propagate_euler(self):
          #define time step of Euler's method
 
-        NperDay = 10
+        NperDay = 1
         sol = np.zeros((3, NperDay+1))
         tSpanFine = np.linspace(0, 1, NperDay+1)
+
+        dailyInfected = 0;
 
         #initiate the values of SIR compartments at the first time point
         sol[:,0] = np.array([self.state[0], self.state[1], self.state[2]])
 
         for i in range(len(tSpanFine)-1):
-            sol[:,i+1] = sol[:,i] + self.RHS(sol[:,i], self.params)/NperDay; 
-            
-        return sol[:,-1]
 
+            dt,new_I = self.RHS(sol[:,i],self.params)
+            dailyInfected =  new_I/NperDay;
+            sol[:,i+1] = sol[:,i] + dt/NperDay;
+
+        print(self.params);
+        print(dailyInfected);
+        print('\n');
+
+        return sol[:,-1],dailyInfected;
 
 
     def propagate(self): 
