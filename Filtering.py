a69cf6e60b3227d0db9dbc85437f53e36bc2a461
Averydx <79957750+Averydx@users.noreply.github.com>
2023-07-28 07:45:56 -0700

    new file:   observations_euler.csv

--- Filtering.py
+++ Filtering.py
@@ -33,22 +33,25 @@ class ParticleFilter:
 
 #calls all internal functions to estimate the parameters
     def estimate_params(self,time): 
-        beta_series = np.zeros(time);
+        betas = [];
         for t in range(time): 
             self.propagate(); 
             temp_weights =  self.resample_with_temp_weights(t); 
             #print(temp_weights);
             self.random_perturbations(); 
-            #self.norm_likelihood(t,temp_weights);
-            beta_series[t] = np.mean(self.particles[t][1]);
-        return beta_series; 
+            mean = 0;
+            for i in range(len(self.particles)):
+                mean += self.particles[i][1]; 
+            mean /= len(self.particles);
+            betas.append(mean);
+        return betas; 
 
     #internal helper function to propagate the particle cloud
     def propagate(self): 
         for i in range(len(self.particles)): 
                 self.Propagrator.params = [self.particles[i][1],self.gamma];
                 self.Propagrator.state = self.particles[i][0]; 
-                temp = self.Propagrator.propagate(); 
+                temp = self.Propagrator.propagate_euler(); 
                 self.particles[i][0]= np.array(temp[0]); 
                 self.dailyInfected[i] = temp[1]; 
 
@@ -57,9 +60,9 @@ class ParticleFilter:
         temp_weights = np.zeros(len(self.particles)); 
         for j in range(len(self.particles)):  
             #temp_weights[j] = (self.weights[j] * (self.dailyInfected[j] ** self.observation_data[t+1])/gamma(self.observation_data[t+1])) * np.exp(-self.dailyInfected[j]);  
-            temp_weights[j] = poisson.pmf(self.observation_data[t+1],self.dailyInfected[j]);
-            # if(temp_weights[j] == 0):
-            #     temp_weights[j] += 10**-300; 
+            temp_weights[j] = self.weights[j] * poisson.pmf(self.observation_data[t+1],self.dailyInfected[j]);
+            if(temp_weights[j] == 0):
+                temp_weights[j] += 10**-300; 
         
         temp_weights = temp_weights/sum(temp_weights); 
 
@@ -69,7 +72,7 @@ class ParticleFilter:
     def resample_with_temp_weights(self,t): 
 
         temp_weights = self.compute_temp_weights(t); 
-        print((temp_weights));
+        #print((temp_weights));
         indexes = np.zeros(len(self.particles));
         for i in range(len(self.particles)):
             indexes[i] = i;
@@ -82,7 +85,7 @@ class ParticleFilter:
     
     #applies the geometric random walk to the particles
     def random_perturbations(self):
-        sigma1 = 0.; 
+        sigma1 = 0; 
         sigma2 = 0.1; 
 
         C = np.array([[((sigma1)**2)/self.population,0,0,0],
@@ -103,34 +106,18 @@ class ParticleFilter:
     
             temp.append(self.particles[i][1]); 
     
-
             temp = np.log(temp); 
             perturbed = np.random.multivariate_normal(mean = temp,cov = C);
-
             perturbed = np.exp(perturbed); 
             s = (sum(perturbed[0:3]));
             for j in range(0,3):
                 perturbed[j] = perturbed[j] / s; 
                 perturbed[j] = perturbed[j] * self.population; 
             
-            self.particles[i] = [(perturbed[0:3]).astype(int),perturbed[3]];
-            
+            self.particles[i] = [perturbed[0:3],perturbed[3]];
     
             print(self.particles[i]);
             print("\n");
-
-    def norm_likelihood(self,t,_temp_weights):
-        new_temp_weights = np.zeros(len(_temp_weights));
-        for i in range(len(_temp_weights)): 
-            new_temp_weights[i] = poisson.pmf(self.observation_data[t+1],self.dailyInfected[i]);
-        
-        
-        new_temp_weights = new_temp_weights/sum(new_temp_weights); 
-
-
-        for i in range(len(_temp_weights)):
-            self.weights[i] = new_temp_weights[i]/_temp_weights[i]; 
-
 
     #function to print the particles in a human readable format
     def print_particles(self):
@@ -148,19 +135,3 @@ class ParticleFilter:
 
 
                  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- NumericalPropagator.py
+++ NumericalPropagator.py
@@ -8,35 +8,43 @@ class one_step_propagator:
         self.state = None;
         self.params = None; 
 
-    def RHS(state,params):
+    def RHS(self,state,params):
     #params has all the parameters – beta, gamma
     #state is a numpy array
 
         S,I,R = state;
-        N = S + I + R; 
 
-        dS = -params[0]*(S*I)/N
-        dI = params[0]*S*I/N-params[1]*I
+
+        new_I = params[0]*S*I/10100;
+
+        dS = -params[0]*(S*I)/10100
+        dI = params[0]*S*I/10100-params[1]*I
         dR = params[1]*I
 
 
 
-        return np.array([dS,dI,dR])
-    
-    def propagate_euler(self): 
+        return np.array([dS,dI,dR]),new_I
+
+    def propagate_euler(self):
          #define time step of Euler's method
 
-        NperDay = 10
+        NperDay = 1
         sol = np.zeros((3, NperDay+1))
         tSpanFine = np.linspace(0, 1, NperDay+1)
+
+        dailyInfected = 0;
 
         #initiate the values of SIR compartments at the first time point
         sol[:,0] = np.array([self.state[0], self.state[1], self.state[2]])
 
         for i in range(len(tSpanFine)-1):
-            sol[:,i+1] = sol[:,i] + self.RHS(sol[:,i], self.params)/NperDay; 
-            
-        return sol[:,-1]
+
+            dt,new_I = self.RHS(sol[:,i],self.params)
+            dailyInfected =  new_I/NperDay;
+            print(dailyInfected);
+            sol[:,i+1] = sol[:,i] + dt/NperDay;
+
+        return sol[:,-1],dailyInfected;
 
 
 
