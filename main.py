da7c050dbbf3bd9964986f350e681c967841abf9
Averydx <79957750+Averydx@users.noreply.github.com>
2023-07-31 12:28:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ def main():
     state = [10000,100,0]; 
     pf = Filtering.ParticleFilter(beta_prior=[0.,1.],
                                   initial_state=state,
-                                  num_particles=10000, 
+                                  num_particles=1000, 
                                   filePath="observations_beta_t.csv");
 
     betas = pf.estimate_params(99);
     end = time.time(); 
     print("The time of execution of the program is :",
-      (end-start) * 10**3, "ms")
-    plt.plot(betas);
+      (end-start), "s")
+    
     real_betas = pd.read_csv('real_beta_t.csv');
-    plt.plot(real_betas.to_numpy());
+    real_betas = np.delete(real_betas,0,1);
+    t = np.linspace(0,100,num=100);
+    plt.scatter(t,real_betas,label="real betas",c="red",s=1.);
+    plt.plot(t,betas,label="predicted betas");
+
+
+    plt.legend(); 
+
+    
     
     plt.show();
 
