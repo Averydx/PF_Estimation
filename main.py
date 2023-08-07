e28bce2526d88947d3815cbe366ff639fd85e047
Averydx <79957750+Averydx@users.noreply.github.com>
2023-08-07 11:31:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-
+from matplotlib import cm
 import Filtering; 
 import pandas as pd; 
 import numpy as np; 
@@ -8,17 +7,17 @@ import time;
 
 def main():
     start = time.time(); 
-    state = [10000,100,0]; 
+    state = [100000,100,0]; 
     pf = Filtering.ParticleFilter(beta_prior=[0.,1.],
-                                  initial_state=state,
+                                  population=100100,
                                   num_particles=5000, 
-                                  filePath="observations_variable_beta.csv");
-
+                                  hyperparamters=[0.01,0.1],
+                                  filePath="observations_variable_beta_poisson.csv");
     aggregate_betas = []; 
     aggregate_dI = []; 
     for _ in range(1): 
         
-      betas,dI = pf.estimate_params(99);
+      betas,dI,qtls = pf.estimate_params(99);
       aggregate_betas.append(betas); 
       aggregate_dI.append(dI); 
 
@@ -27,30 +26,27 @@ def main():
     print("The time of execution of the program is :",
       (end-start), "s")
     
-    real_betas = pd.read_csv('real_beta_variable.csv');
+    real_betas = pd.read_csv('real_variable_beta_poisson.csv');
     real_betas = np.delete(real_betas,0,1);
 
-    t = np.linspace(0,100,num=100);
+    t = np.linspace(0,99,num=99);
 
     plt.figure(figsize=(20, 20))
 
-    fig, (ax1, ax2)  = plt.subplots(1, 2);
-    ax1.scatter(t,real_betas,label="real betas",c="red",s=1.);
-    for i in range(len(aggregate_betas)): 
-      ax1.plot(t,aggregate_betas[i],color="b", alpha=0.1);
-    ax1.legend(); 
-    ax2.scatter(t,pf.observation_data, label="real observations",c="red",s=1.); 
-
-    for i in range(len(aggregate_dI)): 
-      ax2.plot(t,aggregate_dI[i],color="b", alpha=0.1);
+    colors = cm.plasma(np.linspace(0,1,12)); 
 
 
-    ax2.legend();  
-
-    plt.show();
-
+    for i in range(11):
+      plt.fill_between(t, qtls[:,i], qtls[:,22-i], facecolor = colors[11-i], zorder = i);
+      
     
-    
+    plt.xlabel("time(days)"); 
+    plt.ylabel("Number of Infections"); 
+    plt.title("Confidence Intervals"); 
+
+
+
+    plt.show();   
 
 if __name__ == "__main__":
     main()
