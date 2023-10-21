b2ee0872d49b41ec9263a35f9deebdafa8746671
Averydx <avery.drennan@gmail.com>
2023-10-21 11:56:15 -0700

    modified:   ParticleFilter/TimeDependentBeta_test.py 	modified:   jupyter notebooks/JHU_DataExtraction.ipynb 	modified:   output/average_beta.csv

--- ParticleFilter/TimeDependentBeta_test.py
+++ ParticleFilter/TimeDependentBeta_test.py
@@ -15,10 +15,8 @@ real_beta = np.delete(real_beta,0,1)
 
 np.set_printoptions(suppress=True)
 # euler = Euler()
-solver = EulerSolver()
-perturb = MultivariatePerturbations(params={"sigma1":0.2,"sigma2":0.1})
 solver = PoissonSolver()
-perturb = MultivariatePerturbations(params={"sigma1":0.01,"sigma2":0.1})
+perturb = MultivariatePerturbations(params={"sigma1":0.2,"sigma2":0.1})
 resample = NBResample()
 
 algo = TimeDependentAlgo(integrator=solver,
